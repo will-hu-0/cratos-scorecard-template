0b652ee4dd5ffba1398363c9fb9943618502acaf
Will Hu <huwenjian@cn.ibm.com>
2024-11-27 10:11:20 +0800

    feat: add fuzz

---
+++ fuzz_string_compare.js
@@ -0,0 +1,16 @@
+/**
+ * @param { Buffer } data
+ */
+module.exports.fuzz = function (data) {
+	const s = data.toString();
+	if (s.length !== 16) {
+		return;
+	}
+	if (
+		s.slice(0, 8) === "Awesome " &&
+		s.slice(8, 15) === "Fuzzing" &&
+		s[15] === "!"
+	) {
+		throw Error("Welcome to Awesome Fuzzing!");
+	}
+};
